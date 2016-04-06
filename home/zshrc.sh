cbd69304e82ebb904d81525704bdc7a723e4ae40
Greg Losie <glosie@gmail.com>
2016-04-05 20:45:28 -0700

    include gpg-agent config

--- home/zshrc.sh
+++ home/zshrc.sh
@@ -151,3 +151,10 @@ if [[ -s "${ZDOTDIR:-$HOME}/.secrets" ]]; then
   source "${ZDOTDIR:-$HOME}/.secrets"
 fi
 
+# gpg-agent
+if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
+  source ~/.gnupg/.gpg-agent-info
+  export GPG_AGENT_INFO
+else
+  eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+fi
