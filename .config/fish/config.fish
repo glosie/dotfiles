8d6f90586017feb8cc04e8403e8d6d698a6f2773
Greg Losie <glosie@gmail.com>
2021-12-24 15:32:47 -0800

    adjust for running on ubuntu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,11 +12,22 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 
 # PCO
 set -gx AWS_CLI_PATH (which aws)
-bass source ($HOME/Code/pco/bin/pco init - | psub)
-source $HOME/.pcorc
+if test (which bass)    
+    bass source ($HOME/Code/pco/bin/pco init - | psub)
+end
+
+if test -f $HOME/.pcorc
+    source $HOME/.pcorc
+end
 
 # asdf
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+if test (which asdf)
+    if test -f /opt/homebrew/opt/asdf/libexec/asdf.fish
+        source /opt/homebrew/opt/asdf/libexec/asdf.fish
+    else if $HOME/.asdf/asdf.fish
+        source $HOME/.asdf/asdf.fish
+    end
+end
 
 # git
 set -gx GIT_DUET_CO_AUTHORED_BY 1
