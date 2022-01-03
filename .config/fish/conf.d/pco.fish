bdf748b98ec76484a406746bf0d1bc7b3bb00cdb
Greg Losie <greg@pco.bz>
2022-01-03 23:40:46 +0000

    add pco-box to path

--- .config/fish/conf.d/pco.fish
+++ .config/fish/conf.d/pco.fish
@@ -2,6 +2,8 @@
 set -gx AWS_CLI_PATH (which aws)
 test -f $HOME/.pcorc; and source $HOME/.pcorc
 
+fish_add_path $HOME/pco-box/bin
+
 if functions --query bass
     bass source ($HOME/Code/pco/bin/pco init - | psub)
 end
