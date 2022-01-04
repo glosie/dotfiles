a07878eb6f0b6c5bbf5cd29e04516d13c71fff43
Greg Losie <greg@pco.bz>
2022-01-04 10:10:34 -0800

    add box env vars

--- .config/fish/conf.d/pco.fish
+++ .config/fish/conf.d/pco.fish
@@ -1,5 +1,8 @@
 # PCO
-set -gx AWS_CLI_PATH (which aws)
+set -Ux AWS_CLI_PATH (which aws)
+set -Ux MYSQL_PORT_3306_TCP_ADDR 127.0.0.1
+set -Ux MYSQL_READER_PORT_3306_TCP_ADDR 127.0.0.1
+set -Ux MYSQL_READER_PORT_3306_TCP_PORT 3307
 test -f $HOME/.pcorc; and source $HOME/.pcorc
 
 fish_add_path $HOME/pco-box/bin
